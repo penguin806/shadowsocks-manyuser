08ddbd7e9d439e992e016b7b33e4d8c34f33c428
Snow <polarpenguin@foxmail.com>
2019-11-03 11:11:57 +0800

    Save log at /snow/_shadowsocks_log

--- run.sh
+++ run.sh
@@ -2,5 +2,7 @@
 cd `dirname $0`
 eval $(ps -ef | grep "[0-9] python server\\.py m" | awk '{print "kill "$2}')
 ulimit -n 512000
-nohup python server.py m>> /dev/null 2>&1 &
+# nohup python server.py m>> /dev/null 2>&1 &
+mkdir -p /snow/_shadowsocks_log/
+nice -n -15 nohup python server.py m>> /snow/_shadowsocks_log/ssr-`date '+%Y%m%d'`.log 2>&1 &
 
